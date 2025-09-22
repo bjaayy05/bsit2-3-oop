0f194f9345735b44eeb599ee3bf0822122734fd8
bjaayy05 <bsumalinog84951@liceo.edu.ph>
2025-09-22 16:02:20 +0800

    Create Library.java

---
+++ src/Library.java
@@ -0,0 +1,85 @@
+public class Library {
+    private Book[] books;
+    private int bookCount;
+
+
+    public Library() {
+        this.books = new Book[10]; // Max 10 books
+        this.bookCount = 0;
+    }
+
+
+    public boolean addBook(Book book) {
+        if (bookCount < books.length) {
+            books[bookCount++] = book;
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean removeBook(String isbn) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getIsbn().equals(isbn)) {
+                books[i] = books[bookCount - 1]; // Replace with last book
+                books[bookCount - 1] = null; // Remove reference
+                bookCount--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public Book findBook(String isbn) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getIsbn().equals(isbn)) {
+                return books[i];
+            }
+        }
+        return null;
+    }
+
+
+    public boolean borrowBook(String isbn) {
+        Book book = findBook(isbn);
+        if (book != null) {
+            return book.borrowBook();
+        }
+        return false;
+    }
+
+
+    public boolean returnBook(String isbn) {
+        Book book = findBook(isbn);
+        if (book != null) {
+            return book.returnBook();
+        }
+        return false;
+    }
+
+
+    public void displayAvailableBooks() {
+        System.out.println("Available Books:");
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].isAvailable()) {
+                books[i].displayBookInfo();
+                System.out.println("-------------");
+            }
+        }
+    }
+
+
+    public void displayAllBooks() {
+        System.out.println("All Books:");
+        for (int i = 0; i < bookCount; i++) {
+            books[i].displayBookInfo();
+            System.out.println("-------------");
+        }
+    }
+
+
+    public int getBookCount() {
+        return bookCount;
+    }
+}

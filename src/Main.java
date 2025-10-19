8af64f4de388eaabd1ec5bfe88eb1a8d231fa3c1
bjaayy05 <bsumalinog84951@liceo.edu.ph>
2025-10-19 22:22:59 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+
+        PetService Ps = new PetService();
+
+        System.out.println("Pet Service!");
+        System.out.println("---------------------------");
+        System.out.println("Basic checkup: $" + Ps.calculateFee());
+        System.out.println("Checkup with vaccination: $" + Ps.calculateFee(true));
+        System.out.println("Full service: $" + Ps.calculateFee(true, true));
+        System.out.println("Emergency: $" + Ps.calculateFee("Critical"));
+        System.out.println();
+
+        System.out.println("Welcome to the Pet Clinic!");
+        System.out.println("---------------------------");
+        Pet Dog = new Dog("Buddy", 3);
+        Pet Cat = new Cat("Whiskers", 2);
+        Pet Bird = new Bird("Tweety", 1);
+
+        Dog.displayInfo(); Dog.makeSound();
+        System.out.println();
+
+        Cat.displayInfo(); Cat.makeSound();
+        System.out.println();
+
+        Bird.displayInfo(); Bird.makeSound();
+        System.out.println();
+
+        System.out.println("Training Session Started!");
+        System.out.println("---------------------------");
+        Trainable trainDog = new Dog("Buddy", 3);
+        Trainable trainBird = new Bird("Tweety", 1);
+        trainDog.performTrick();
+        trainBird.performTrick();
     }
 }
